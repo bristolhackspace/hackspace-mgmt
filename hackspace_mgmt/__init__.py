54dac66e99e14925be65e54565c33615492e2227
Sam <633856+SamP20@users.noreply.github.com>
2023-03-12 18:27:06 +0000

    Initial Flask app

---
+++ hackspace_mgmt/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask, render_template
+from flask_admin import Admin
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+    )
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+
+    return app
