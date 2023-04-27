821414eec154c3764811640f9111ba25cc1ac962
SamP20 <633856+SamP20@users.noreply.github.com>
2023-04-27 19:15:41 +0100

    Remove Flask-Migrate

--- hackspace_mgmt/database.py
+++ hackspace_mgmt/database.py
@@ -1,9 +1,6 @@
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 def init_db(app):
     db.init_app(app)
-    migrate.init_app(app, db)
