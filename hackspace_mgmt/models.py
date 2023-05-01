1bc70d9bd44b8321345ecfa8be374cf5114329b4
SamP20 <633856+SamP20@users.noreply.github.com>
2023-05-01 17:14:21 +0100

    Update models

--- hackspace_mgmt/models.py
+++ hackspace_mgmt/models.py
@@ -9,16 +9,22 @@ db = SQLAlchemy()
 
 
 class InductionState(enum.Enum):
-    valid = 1
-    expired = 2
-    banned = 3
+    valid = "valid"
+    expired =  "expired"
+    banned =  "banned"
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Member(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     discourse_id: Mapped[Optional[int]] = mapped_column(unique=True)
-    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    preferred_name: Mapped[str] = mapped_column(String(200), nullable=False)
     cards: Mapped[List["Card"]] = relationship(back_populates="member")
+    inductions: Mapped[List["Induction"]] = relationship(back_populates="member")
+
+    def __str__(self):
+        return self.preferred_name
 
 class Card(db.Model):
     card_id: Mapped[int] = mapped_column(primary_key=True)
@@ -26,10 +32,17 @@ class Card(db.Model):
     member_id: Mapped[int] = mapped_column(ForeignKey("member.id"))
     member: Mapped["Member"] = relationship(back_populates="cards")
 
+    def __str__(self):
+        return f"#{self.card_id}"
+
 class Machine(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     controllers: Mapped[List["MachineController"]] = relationship(back_populates="machine")
+    inductions: Mapped[List["Induction"]] = relationship(back_populates="machine")
+
+    def __str__(self):
+        return self.name
 
 class MachineController(db.Model):
     mac: Mapped[int] = mapped_column(primary_key=True)
@@ -37,8 +50,13 @@ class MachineController(db.Model):
     machine: Mapped["Machine"] = relationship(back_populates="controllers")
     requires_update: Mapped[bool] = mapped_column()
 
+    def __str__(self):
+        return hex(self.mac)
+
 class Induction(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     member_id: Mapped[int] = mapped_column(ForeignKey("member.id"))
     machine_id: Mapped[int] = mapped_column(ForeignKey("machine.id"))
     state: Mapped[InductionState] = mapped_column(nullable=False)
+    member: Mapped["Member"] = relationship(back_populates="inductions")
+    machine: Mapped["Machine"] = relationship(back_populates="inductions")
