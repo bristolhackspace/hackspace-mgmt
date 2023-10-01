0d0c553c90efd2fb94e9f9b647fa67edfda3d2dd
Sam <633856+SamP20@users.noreply.github.com>
2023-10-01 15:58:53 +0100

    Remove dedicated new member form

--- hackspace_mgmt/admin/member.py
+++ hackspace_mgmt/admin/member.py
@@ -73,6 +73,9 @@ class MemberView(ViewHelperJsMixin, ModelView):
         }
     }
 
+    form_create_rules = ('first_name', 'last_name', 'preferred_name', 'email', 'cards')
+
+
     def search_placeholder(self):
         return "Member name or email"
 
@@ -80,68 +83,6 @@ class MemberView(ViewHelperJsMixin, ModelView):
         return request.headers.get("X-Remote-User") == "admin"
 
 
-class NewMemberForm(form.BaseForm):
-    first_name = fields.StringField(
-        validators=[DataRequired()],
-        render_kw={"autocomplete": "off"}
-    )
-    last_name = fields.StringField()
-    preferred_name = fields.StringField(
-        column_labels["preferred_name"],
-        description=column_descriptions["preferred_name"],
-        render_kw={
-            "placeholder": "(optional)",
-            "autocomplete": "off"
-        }
-    )
-    email = fields.EmailField(
-        validators=[DataRequired()],
-        render_kw={"autocomplete": "off"}
-    )
-    card_number = fields.IntegerField(
-        'Number on front of card/keyfob',
-        render_kw={"autocomplete": "off"}
-    )
-
-class NewMemberView(BaseView):
-
-    @expose('/', methods=('GET', 'POST'))
-    def index(self):
-        return_url = get_redirect_target() or self.get_url('.index')
-
-        member_form = NewMemberForm(request.form)
-
-        if validate_form_on_submit(member_form):
-            new_member = Member(
-                first_name = member_form.first_name.data,
-                last_name = member_form.last_name.data,
-                preferred_name = member_form.preferred_name.data,
-                email = member_form.email.data,
-            )
-            db.session.add(new_member)
-            db.session.commit()
-
-            card_update = db.update(Card).where(Card.number_on_front==member_form.card_number.data)
-            card_update = card_update.values(member_id=new_member.id)
-            result = db.session.execute(card_update)
-            if not result.rowcount:
-                card = Card(
-                    number_on_front=member_form.card_number.data,
-                    member_id=new_member.id
-                )
-                db.session.add(card)
-                card_type = "unenrolled"
-            else:
-                card_type = "pre-enrolled"
-            db.session.commit()
-
-            flash(f'New member created with {card_type} card.', 'success')
-            return redirect(return_url)
-
-        return self.render('admin/new_member.html', return_url=return_url, form=member_form)
-
-
 def create_views(admin: Admin):
     admin.add_view(MemberView(Member, db.session, category="Membership"))
-    admin.add_view(NewMemberView("New Member", endpoint="new_member", category="Membership"))
 
