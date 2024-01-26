59e5e8dfc556f266255a6d48b32610f61561c56f
SamP20 <633856+SamP20@users.noreply.github.com>
2024-01-26 15:34:04 +0000

    Fix machine hostname migration SQL

--- migration/15_machine_hostname.sql
+++ migration/15_machine_hostname.sql
@@ -1,5 +1,6 @@
 ALTER TABLE IF EXISTS public.machine_controller
-    ADD COLUMN hostname  NOT NULL character varying(255);
+    ADD COLUMN hostname character varying(255) NOT NULL DEFAULT '';
+UPDATE public.machine_controller SET hostname = mac WHERE hostname = '';
 ALTER TABLE IF EXISTS public.machine_controller
     ADD UNIQUE (hostname);
 ALTER TABLE IF EXISTS public.machine_controller DROP COLUMN IF EXISTS mac;
