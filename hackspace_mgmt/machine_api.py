4a3a1468d1809b7ad4de638ab4dbf1f3e3ef2957
SamP20 <633856+SamP20@users.noreply.github.com>
2024-03-09 13:09:45 +0000

    Add machine enroll endpoint

--- hackspace_mgmt/machine_api.py
+++ hackspace_mgmt/machine_api.py
@@ -4,6 +4,8 @@ from flask import (
 )
 from .models import db, Member, Card, Machine, MachineController, Induction, InductionState
 from sqlalchemy.exc import NoResultFound
+from sqlalchemy.dialects.postgresql import insert
+from datetime import date
 import logging
 
 bp = Blueprint('machine_api', __name__, url_prefix='/api/machines')
@@ -15,20 +17,24 @@ def controller_from_hostname(hostname, join_machine=True):
     return db.one_or_404(machine_controller_query)
 
 
+def format_card_serial(card_id_str):
+    if card_id_str is None:
+        card_id_str = "00000000"
+    if "-" in card_id_str:
+        parts = card_id_str.split("-")
+        card_id_str = "".join(parts[::-1])
+
+    current_app.logger.info(f"Serial string is {card_id_str}")
+    return int(card_id_str, 16)
+
+
 @bp.route('/<hostname>/unlock')
 def unlock(hostname):
     controller = controller_from_hostname(hostname)
 
     current_app.logger.debug(f"Controller is {controller}")
 
-    card_id_str = request.args.get("card_id", "00000000")
-    if "-" in card_id_str:
-        parts = card_id_str.split("-")
-        card_id_str = "".join(parts[::-1])
-
-    current_app.logger.info(f"Serial string is {card_id_str}")
-
-    card_serial = int(card_id_str, 16)
+    card_serial = format_card_serial(request.args.get("card_id"))
     card_subq = db.select(Card).where(Card.card_serial==card_serial).subquery()
     member_query = db.select(Member).join(card_subq, Member.id==card_subq.c.member_id)
     member = db.one_or_404(member_query)
@@ -41,15 +47,13 @@ def unlock(hostname):
             db.select(Induction)
                 .where(Induction.member_id==member.id)
                 .where(Induction.machine_id==controller.machine.id)
+                .where(Induction.state==InductionState.valid)
         ).scalar_one()
         current_app.logger.debug(f"Inductions: {induction}")
 
         current_app.logger.info(f"'{member}' ({member.id}) successfully unlocked {controller.machine.name} ({hostname})")
     except NoResultFound:
         current_app.logger.info(f"'{member}' ({member.id}) unauthorized unlock attempt for {controller.machine.name} ({hostname})")
-        abort(403)
-
-    if induction.state != InductionState.valid:
         abort(403)
 
     return {"unlocked": True}
@@ -59,6 +63,66 @@ def lock(hostname):
     controller = controller_from_hostname(hostname)
     current_app.logger.info(f"Lock button pressed on {controller.machine.name} ({hostname})")
     return {"unlocked": False}
+
+@bp.route('/<hostname>/enroll')
+def enroll(hostname):
+    controller = controller_from_hostname(hostname)
+
+    current_app.logger.debug(f"Controller is {controller}")
+
+    inductor_card = format_card_serial(request.args.get("inductor_id"))
+    card_subq = db.select(Card).where(Card.card_serial==inductor_card).subquery()
+    member_query = db.select(Member).join(card_subq, Member.id==card_subq.c.member_id)
+    inductor = db.one_or_404(member_query)
+
+    current_app.logger.debug(f"Inductor is {inductor}")
+    current_app.logger.debug(f"Machine ID is {controller.machine.id}")
+
+    try:
+        induction = db.session.execute(
+            db.select(Induction)
+                .where(Induction.member_id==inductor.id)
+                .where(Induction.machine_id==controller.machine.id)
+                .where(Induction.state==InductionState.valid)
+                .where(Induction.can_induct==True)
+        ).scalar_one()
+
+        current_app.logger.info(f"'{inductor}' ({inductor.id}) authorised to induct for {controller.machine.name} ({hostname})")
+    except NoResultFound:
+        current_app.logger.info(f"'{inductor}' ({inductor.id}) unauthorized to induct for {controller.machine.name} ({hostname})")
+        abort(403)
+
+    inductee_card_str = request.args.get("inductee_id")
+    if inductee_card_str is None:
+        return {}
+
+    inductee_card = format_card_serial(inductee_card_str)
+    card_subq = db.select(Card).where(Card.card_serial==inductee_card).subquery()
+    member_query = db.select(Member).join(card_subq, Member.id==card_subq.c.member_id)
+    inductee = db.one_or_404(member_query)
+
+
+    today=date.today()
+    upsert_stmt = insert(Induction).values(
+        member_id=inductee.id,
+        machine_id=controller.machine.id,
+        state=InductionState.valid,
+        inducted_by=inductor.id,
+        inducted_on=today
+    ).on_conflict_do_update(
+        index_elements=[Induction.member_id, Induction.machine_id],
+        set_=dict(
+            state=InductionState.valid,
+            inducted_by=inductor.id,
+            inducted_on=today
+        ),
+    )
+    db.session.execute(upsert_stmt)
+    db.session.commit()
+
+    return {}
+
+
 
 @bp.route('/<hostname>/status', methods=["POST"])
 def status(hostname):
