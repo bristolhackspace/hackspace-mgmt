b5b881ba3cada90d72867c840a9dd30a769f24e7
SamP20 <633856+SamP20@users.noreply.github.com>
2025-10-04 15:46:56 +0100

    Storage label date limits

--- hackspace_mgmt/general/label.py
+++ hackspace_mgmt/general/label.py
@@ -17,6 +17,8 @@ bp = Blueprint("label", __name__)
 
 logger = logging.Logger(__name__)
 
+LATEST_SHORT_STAY = date(year=2025, month=10, day=3)
+
 @bp.route("/label")
 @login_required
 def index():
@@ -31,9 +33,12 @@ def create():
 
     if label_type == "short_stay":
         expiry = now + timedelta(days=30)
+        if expiry > LATEST_SHORT_STAY:
+            expiry = LATEST_SHORT_STAY
+        
         caption = "Short stay"
     elif label_type == "project_box":
-        expiry = now + timedelta(days=30*6)
+        expiry = now + timedelta(days=30*3)
         caption = "Project box"
     else:
         return {"error": "unknown label type"}, 400
